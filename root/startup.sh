5b17f774643f2052aeb72228ac5463c9b55997d6
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-09 15:34:10 -0800

    Basic DDNS

--- root/startup.sh
+++ root/startup.sh
@@ -8,6 +8,23 @@ fi
 
 IP=$(ip addr show dev ${IFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
 
+ddns_names=""
+last_external_ip=""
+function ddns() {
+  sleep 1 &
+  while wait "$!"; do
+    external_ip=$(upnpc -m ${IFACE} -s | grep ExternalIPAddress | sed "s/^.*= //")
+    if [ "${external_ip}" != "${last_external_ip}" ]; then
+      last_external_ip=${external_ip}
+      for name in $ddns_names; do
+        url=$(echo $DDNS_URL | sed "s/{{IP}}/${IP}/g" | sed "s/{{HOSTNAME}}/@/g" | sed "s/{{DOMAINNAME}/${name}/g")
+        wget --no-check-certificate -q -O - "${url}"
+      done
+    fi
+    sleep 1800 &
+  done
+}
+
 for website in ${WEBSITES}; do
   site=$(echo $website | cut -d"#" -f 1)
   port=$(echo $website | cut -d"#" -f 2)
@@ -30,6 +47,8 @@ server {
       echo "${IP} ${gsite}" >> /etc/dnshosts.d/hosts.conf
       if [ "$(echo ${gsite} | grep '\.')" = "" ]; then
         echo "${IP} ${gsite}.${__DOMAINNAME}" >> /etc/dnshosts.d/hosts.conf
+      else
+        ddns_names="${dds_names} ${gsite}"
       fi
     done
   fi
@@ -37,6 +56,9 @@ done
 
 nginx
 dnsmasq
+if [ "${DDNS_URL}" != "" ]; then
+  ddns &
+fi
 
 # Run until stopped
 trap "nginx quit ; killall dnsmasq ; exit" TERM INT
