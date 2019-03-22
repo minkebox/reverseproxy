d0d63f0d4894bc91d606cac661cda0f8f26b95bc
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-21 18:17:10 -0700

    Remove unused DDNS code

--- root/startup.sh
+++ root/startup.sh
@@ -11,25 +11,6 @@ IP=$(ip addr show dev ${IFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | he
 touch /etc/dnshosts.d/hosts.conf
 dnsmasq
 
-ddns_names=""
-last_external_ip=""
-function ddns() {
-  sleep 1 &
-  while wait "$!"; do
-    external_ip=$(upnpc -m ${IFACE} -s | grep ExternalIPAddress | sed "s/^.*= //")
-    if [ "${external_ip}" != "${last_external_ip}" ]; then
-      last_external_ip=${external_ip}
-      for name in $ddns_names; do
-        hostname=$(echo $name | cut -d'#' -f 1)
-        domainname=$(echo $name | cut -d'#' -f 2-)
-        url=$(echo $DDNS_URL | sed "s/{{IP}}/${external_ip}/g" | sed "s/{{HOSTNAME}}/${hostname}/g" | sed "s/{{DOMAINNAME}}/${domainname}/g")
-        wget --no-check-certificate -S -O - "${url}"
-      done
-    fi
-    sleep 1800 &
-  done
-}
-
 # By default, if we cannot identify the correct server, we 404.
 echo "
 server {
@@ -61,16 +42,6 @@ server {
       echo "${IP} ${gsite}" >> /etc/dnshosts.d/hosts.conf
       if [ "$(echo ${gsite} | grep '\.')" = "" ]; then
         echo "${IP} ${gsite}.${__DOMAINNAME}" >> /etc/dnshosts.d/hosts.conf
-      elif [ "${DDNS_URL}" != "" ]; then
-        hostname=$(echo ${gsite} | cut -d'.' -f 1)
-        domainname=$(echo ${gsite} | cut -d'.' -f 2-)
-        if [ "$(echo ${domainname} | grep '\.')" = "" ]; then
-          ddns_names="${dds_names} #${hostname}.${domainname}"
-        elif [ "$(whois ${domainname} | grep 'NOT FOUND')" != "" ]; then
-          ddns_names="${dds_names} #${hostname}.${domainname}"
-        else
-          ddns_names="${dds_names} ${hostname}#${domainname}"
-        fi
       fi
     done
   fi
@@ -100,9 +71,6 @@ server {
 done
 
 nginx
-if [ "${ddns_names}" != "" ]; then
-  ddns &
-fi
 
 # Run until stopped
 trap "nginx -s quit ; killall dnsmasq ; exit" TERM INT
